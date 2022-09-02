fec7a5e8b6a85a46377482560dcb15a65143e092
Chirag Rupani <36813124+chiragrupani@users.noreply.github.com>
2022-09-02 08:35:39 +0530

    Fixed source map warnings #4

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,41 @@
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import replace from '@rollup/plugin-replace';
-import typescript from '@rollup/plugin-typescript';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
+import typescript from "@rollup/plugin-typescript";
+import { defineConfig } from "rollup";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+
 // import postcss from 'rollup-plugin-postcss';
 
-const packageJson = require('./package.json');
+const packageJson = require("./package.json");
 const env = process.env.NODE_ENV;
 
-export default {
-  input: 'src/index.ts',
+export default defineConfig({
+  input: "src/index.ts",
   output: [
     {
       file: packageJson.main,
-      format: 'cjs',
-      sourcemap: true,
+      format: "cjs",
+      sourcemap: "inline",
     },
     {
       file: packageJson.module,
-      format: 'esm',
-      sourcemap: true,
+      format: "esm",
+      sourcemap: "inline",
     },
   ],
   plugins: [
     peerDepsExternal(),
     resolve(),
     replace({
-      values: { 'process.env.NODE_ENV': JSON.stringify(env) },
+      values: { "process.env.NODE_ENV": JSON.stringify(env) },
       preventAssignment: true,
     }),
     commonjs(),
     typescript({
       declaration: true,
-      declarationDir: ".",     
-      tsconfig: './tsconfig.json'
+      declarationDir: ".",
+      tsconfig: "./tsconfig.json",
     }),
     // postcss(),
     // terser({
@@ -42,4 +44,4 @@ export default {
     //   },
     // }),
   ],
-};
+});
