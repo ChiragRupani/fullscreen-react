59cb759803aca7d0617a95d9a58e4b0f47fdbed7
Chirag Rupani <36813124+ChiragRupani@users.noreply.github.com>
2024-06-18 20:17:38 +0530

    Updated dependencies

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,12 +2,25 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import replace from "@rollup/plugin-replace";
 import typescript from "@rollup/plugin-typescript";
+import { readFileSync } from "node:fs";
 import { defineConfig } from "rollup";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 // import postcss from 'rollup-plugin-postcss';
 
-const packageJson = require("./package.json");
+// import * as packageJson from "./package.json" assert { type: "json" };
+
+// const packageJson = require("./package.json");
+
+const packageJson = JSON.parse(
+  readFileSync(new URL("./package.json", import.meta.url))
+);
+
+// const packageJson = {
+//   main: "build/index.js",
+//   module: "build/index.esm.js",
+// };
+
 const env = process.env.NODE_ENV;
 
 export default defineConfig({
