77fe7b88c91e2b22f13ce1716673c6d9ee248c8d
Chirag Rupani <36813124+ChiragRupani@users.noreply.github.com>
2025-08-02 09:27:04 +0530

    Removed unused dirname

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,4 @@
-import { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   experimental: { enableNativePlugin: true },
